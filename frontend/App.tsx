49d91f9c4f674df82e8c9e502c880448e4062b58
David De <david@getzuma.com>
2022-02-26 01:16:41 -0800

    Added Amplify + Cognito based user authentication.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,13 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, ScrollView, FlatList } from 'react-native';
 import { ListItem, Button, Text, colors, withTheme } from 'react-native-elements';
 import * as ImagePicker from 'expo-image-picker';
+import Amplify, { API, Auth } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+const COUPON_REMINDERS_BACKEND_NAME = "CouponRemindersBackend";
+const COUPON_REMINDERS_BACKEND_ADD_COUPON_PATH   = "/addCoupon";
+const COUPON_REMINDERS_BACKEND_GET_COUPONS_PATH  = "/getCoupons";
+
+Amplify.configure({
+    Auth: {
+        region: 'us-west-1',
+        userPoolId: 'us-west-1_s4rOJ0zxo',
+        userPoolWebClientId: '70d9qekok8i0r4vc9dbpntq29m',
+        oauth: {
+            domain: 'service-user-pool-domain-stagging-coupon-reminders.auth.us-west-1.amazoncognito.com',
+            scope: ['phone', 'email', 'openid', 'aws.cognito.signin.user.admin', 'profile'],
+            redirectSignIn: 'http://localhost:3000',
+            redirectSignOut: 'http://localhost:3000',
+            responseType: 'code'
+        }
+    },
+    API: {
+        endpoints: [{
+            name: COUPON_REMINDERS_BACKEND_NAME,
+            endpoint: "https://t38hqd6fq0.execute-api.us-west-1.amazonaws.com"
+        }]
+    },
+    Analytics: {
+        disabled: true,
+    }
+});
 
 type AppProps = {};
-
-const COUPON_REMINDERS_BACKEND              = "https://h8lwm7xnpi.execute-api.us-west-1.amazonaws.com/"
-const COUPON_REMINDERS_BACKEND_ADD_COUPON   = COUPON_REMINDERS_BACKEND + "addCoupon";
-const COUPON_REMINDERS_BACKEND_GET_COUPONS  = COUPON_REMINDERS_BACKEND + "getCoupons";
-const AUTH_TOKEN = 'Bearer ' + 'eyJraWQiOiJzRk1cL1hQVldyY3V5cjBpVk1TaE5SZnIyNFFlakJ2bGgyVTAwaVNKTEVGdz0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoiOUVURHpBdzBaZVFWTzFRekFwX0RWQSIsInN1YiI6IjJjMDU4ZjNlLTA0ZWItNDhkMS1iOWQxLTVhMGZkYzBmYjRmYSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtd2VzdC0xLmFtYXpvbmF3cy5jb21cL3VzLXdlc3QtMV85bmNPcTJRejUiLCJjb2duaXRvOnVzZXJuYW1lIjoiMmMwNThmM2UtMDRlYi00OGQxLWI5ZDEtNWEwZmRjMGZiNGZhIiwiYXVkIjoiNTV2M3M5YTR0cGk1YmlrMmFtcWI5MW11NnIiLCJldmVudF9pZCI6ImVkOGQ4MTI5LWY4ZjctNGYyMC1hYWZlLWJiMTI5OGEzNjdjOSIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjQ1NDkzMDE4LCJleHAiOjE2NDU0OTY2MTgsImlhdCI6MTY0NTQ5MzAxOCwianRpIjoiOWQxNGYwZWItN2Y5My00ZmM3LWJiN2UtYWM1NzhlMzdlZGE0IiwiZW1haWwiOiJkYXZpZGRlY29kaW5nQGdtYWlsLmNvbSJ9.oa5bCxEu-QQOFG165U5Si1FISvRSDixdSN8Clg7m__rd8f9C_KWeadV_7RgKMyglK6bkXKgNIYGmzL7iI2XaO9fFBW04DOfS3byi6Y4aEaQAMgAJuGGqzIgSizpnKDa5eO5n4NJ7qlwshVn-cek0YaR4g7DZF0vyav0mdL1iknachmDc33yZOnwxFi_Z97AZQDikKlDRobJjDqgemAvAgsnO040SHEYPNQDXfEeonBCkNOPZ9rCeWhf60fMsnj_nck7FD61imPC4S95RZr7O19m-MNf-NQCJltkXDLRgRZQRcArsVf3_AcT1IwOzKVTKPFoPz1xKyR9NEcOo-5LB0Q';
 
 const App: React.FunctionComponent<AppProps> = () => {
     // 1. Storing the image in memory.
@@ -46,25 +71,17 @@ const App: React.FunctionComponent<AppProps> = () => {
             })
             .catch(error => console.log(error));
 
-        const s3URLRequestOptions = {
-            method: 'POST',
+        const addCouponRequestOptions = {
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': AUTH_TOKEN
+                'Authorization': `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`
             },
-            body: JSON.stringify({ "user_id": "test" })
+            body: { "user_id": "test" }
         }
-        const s3URL = await fetch(COUPON_REMINDERS_BACKEND_ADD_COUPON, s3URLRequestOptions)
+        const s3URL = await API
+            .post(COUPON_REMINDERS_BACKEND_NAME, COUPON_REMINDERS_BACKEND_ADD_COUPON_PATH, addCouponRequestOptions)
             .then(async response => {
-                const isJson = response.headers.get('content-type')?.includes('application/json');
-                const data = isJson && await response.json();
-                const s3URL = data['url'];
-
-                if (!response.ok) {
-                    const error = (data && data.message) || response.status;
-                    return Promise.reject(error);
-                }
-                return s3URL;
+                return response['url'];
             })
             .catch(error => console.error(error));
 
@@ -81,8 +98,6 @@ const App: React.FunctionComponent<AppProps> = () => {
                     const error = (data && data.message) || response.status;
                     return Promise.reject(error);
                 }
-
-                console.log(data);
             })
             .catch(error => console.error(error));
     };
@@ -91,15 +106,17 @@ const App: React.FunctionComponent<AppProps> = () => {
     const [couponList, setCouponList] = useState([]);
 
     useEffect(async () => {
-        const requestOptions = {
-            method: 'GET',
+        const getCouponsRequestOptions = {
             headers: {
-                'Authorization': AUTH_TOKEN
+                'Authorization': `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`
             }
         };
-        const response = await fetch(COUPON_REMINDERS_BACKEND_GET_COUPONS + "/test", requestOptions);
-        const data = await response.json();
-        setCouponList(data['coupons']);
+        await API
+            .get(COUPON_REMINDERS_BACKEND_NAME, COUPON_REMINDERS_BACKEND_GET_COUPONS_PATH + "/test", getCouponsRequestOptions)
+            .then(async response => {
+                setCouponList(response['coupons']);
+            })
+            .catch(error => console.log(error));
     });
 
 
@@ -178,4 +195,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default withTheme(App, '');
+export default withTheme(withAuthenticator(App), '');
